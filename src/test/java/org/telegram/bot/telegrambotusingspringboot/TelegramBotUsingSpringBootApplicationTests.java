27778772d8dc26ea486babbb39deec91ed02f922
Ismoil <ismoilzaripov55@gmail.com>
2023-06-27 19:23:38 +0500

    first commit

---
+++ src/test/java/org/telegram/bot/telegrambotusingspringboot/TelegramBotUsingSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telegram.bot.telegrambotusingspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelegramBotUsingSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
